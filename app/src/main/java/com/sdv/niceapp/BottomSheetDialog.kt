fe7e3b8c4bbf09e17517293abf891ae526b55e57
sergd <serg.dv.cas@gmail.com>
2019-04-04 23:02:17 +0300

    Added bottom navigation bar

---
+++ app/src/main/java/com/sdv/niceapp/BottomSheetDialog.kt
@@ -0,0 +1,29 @@
+package com.sdv.niceapp
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.bottom_sheet_layout.*
+
+class BottomSheetDialog : BottomSheetDialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.bottom_sheet_layout, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        bottom_navigation_view.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav1 -> {
+                    Log.d("", "")
+                }
+            }
+            true
+        }
+    }
+}
--- app/src/main/java/com/sdv/niceapp/MainActivity.kt
+++ app/src/main/java/com/sdv/niceapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.sdv.niceapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.sdv.niceapp.databinding.MainLayoutBinding
+import kotlinx.android.synthetic.main.main_layout.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +17,28 @@ class MainActivity : AppCompatActivity() {
         val binding: MainLayoutBinding = DataBindingUtil.setContentView(this, R.layout.main_layout)
         binding.activity = this
 
+        setSupportActionBar(bottom_app_bar)
     }
 
     fun onFabClick(view: View) {
         Log.d("", "")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bottom_app_bar, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> {
+                Log.d("", "item = $item")
+                BottomSheetDialog().show(supportFragmentManager, null)
+            }
+        }
+
+        return true
     }
 }

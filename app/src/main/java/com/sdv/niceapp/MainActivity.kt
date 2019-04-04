b8355e990e20cc410ef5d215233ef3339a8718f4
sergd <serg.dv.cas@gmail.com>
2019-04-05 00:18:28 +0300

    Implemented simple navigation scheme from home fragment to search one

--- app/src/main/java/com/sdv/niceapp/MainActivity.kt
+++ app/src/main/java/com/sdv/niceapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sdv.niceapp
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -7,6 +8,10 @@ import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.transaction
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.sdv.niceapp.databinding.MainLayoutBinding
 import kotlinx.android.synthetic.main.main_layout.*
 
@@ -21,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onFabClick(view: View) {
-        Log.d("", "")
+        Navigation.findNavController(this@MainActivity, R.id.fragment).navigate(R.id.action_global_searchFragment)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {

4bac73648709577bdab12856ee5e1a4165047556
sergd <serg.dv.cas@gmail.com>
2019-04-04 20:53:28 +0300

    Added click listener in databinding on fab

--- app/src/main/java/com/sdv/niceapp/MainActivity.kt
+++ app/src/main/java/com/sdv/niceapp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.sdv.niceapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.sdv.niceapp.databinding.MainLayoutBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_layout)
+        val binding: MainLayoutBinding = DataBindingUtil.setContentView(this, R.layout.main_layout)
+        binding.activity = this
+
+    }
+
+    fun onFabClick(view: View) {
+        Log.d("", "")
     }
 }

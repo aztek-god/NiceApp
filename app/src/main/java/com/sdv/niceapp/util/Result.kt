7e49eddd8d4a4ce1be4b72d9e5eb561cb1c01d28
sergd <serg.dv.cas@gmail.com>
2019-04-06 19:39:33 +0300

    Work on pagination

--- app/src/main/java/com/sdv/niceapp/util/Result.kt
+++ app/src/main/java/com/sdv/niceapp/util/Result.kt
@@ -5,14 +5,14 @@ sealed class Result<T> {
 
     data class Error<T>(val e: Throwable) : Result<T>()
 
-    data class Data<T>(val data: T, val isCompleted: Boolean = false) : Result<T>()
+    data class Data<T>(val data: T) : Result<T>()
 
     class Empty<T> : Result<T>()
 
     companion object {
         fun <T> createProgress(isLoading: Boolean) = Progress<T>(isLoading)
         fun <T> createError(e: Throwable) = Error<T>(e)
-        fun <T> createData(data: T, isCompleted: Boolean) = Data(data, isCompleted)
+        fun <T> createData(data: T) = Data(data)
         fun <T> createEmpty() = Empty<T>()
     }
 }
--- app/src/main/java/com/sdv/niceapp/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/sdv/niceapp/viewmodel/HomeViewModel.kt
@@ -9,23 +9,39 @@ import com.sdv.niceapp.util.Result
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
+private const val INIT_PAGE_INDEX = 1
+private const val STANDARD_PAGE_SIZE = 10
+
 class HomeViewModel(private val topHeadlinesService: TopHeadlinesService) : AbstractCompositeViewModel() {
+
+    private var currentPage = INIT_PAGE_INDEX
 
     private val mArticleLiveData: MutableLiveData<Result<List<Article>>> = MutableLiveData()
     val articleLiveData: LiveData<Result<List<Article>>> get() = mArticleLiveData
 
     fun loadTopHeadlineNews() {
         val disposable = topHeadlinesService
-            .getTopHeadlines(country = "us")
-            .map { it.articles }
+            .getTopHeadlines(country = "us", page = 4)
+            .map { response ->
+                val result = if (response.articles.isEmpty()) {
+                    Result.createEmpty<List<Article>>()
+                } else {
+                    Result.createData(response.articles)
+                }
+                ArticleResult(response.totalResults, result)
+            }
+            .doOnError { currentPage = INIT_PAGE_INDEX }
+            .doOnNext { currentPage++ }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
-            .subscribe({ response: List<Article> ->
-                mArticleLiveData.value = Result.createData(response, false)
+            .subscribe({ response: ArticleResult ->
+                mArticleLiveData.value = response.result
             }, { error ->
                 mArticleLiveData.value = Result.createError(error)
             })
 
         addDisposable(disposable)
     }
+
+    data class ArticleResult(val totalAmount: Int, val result: Result<List<Article>>)
 }
